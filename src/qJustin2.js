a65a37f864c5494df8cf37eb29942158f7c89ce2
wyk1984 <132546564+wyk1984@users.noreply.github.com>
2023-05-04 14:56:20 +0800

    re-do the change Q2

--- src/qJustin2.js
+++ src/qJustin2.js
@@ -1,10 +1,8 @@
 /*
     Task 1: 
     - Declare an array that are going to be used to store patient's name.
-
     Task 2: 
     - Add code to add patient's name into the array declared in task 1.
-
     Task 3: 
     - Implement listPatient() function to print all patient's name stored in the array
 */
@@ -16,6 +14,12 @@ function addPatient(patientName){
     // Task 2: Add code here
 }
 
+recallPatientName = [];
+addPatient = function (patientName) {
+  // Task 2: Add code here
+  recallPatientName.push(patientName);
+  return recallPatientName;
+};
 // Task 3: Add code here
 
 
@@ -23,4 +27,8 @@ addPatient("John");
 addPatient("Mary");
 addPatient("Mark");
 
+listPatient(); // This should list ["John", "Mary", "Mark"]
+listPatient = function () {
+  return recallPatientName;
+};
 listPatient(); // This should list ["John", "Mary", "Mark"]
