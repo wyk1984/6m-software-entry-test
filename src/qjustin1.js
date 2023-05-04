7c8576cf84276a1f74165f84582e9b94ed3d7ea0
wyk1984 <132546564+wyk1984@users.noreply.github.com>
2023-05-04 14:53:01 +0800

    re-do the change
    -

---
+++ src/qjustin1.js
@@ -0,0 +1,37 @@
+/*
+    Task 1
+    - Create a function that would swap the value of x and y using only x and y as variables.
+    - x and y must be numeric.
+    - return -1 if x and y is not numeric.
+    - print the swapped values in the console
+    Task 2
+/*
+    Task 1
+    - Create a function that would swap the value of x and y using only x and y as variables.
+    - x and y must be numeric.
+    - return -1 if x and y is not numeric.
+    - print the swapped values in the console
+    Task 2
+    - invoke the function "swap"
+*/
+
+function swap(x, y){
+swap = function (x, y) {
+  if (isNaN(x) || isNaN(y) == true) {
+    return -1;
+  } else {
+    // Task 1: Add code here
+}
+    swapPosition = [y, x];
+    return swapPosition;
+  }
+};
+
+// Task 2: Add code here
+swap(100, 800);
+
+swap("Tiger", 800);
+
+swap(100, "Elephant");
+
+module.exports = swap;
