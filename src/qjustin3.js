082d5b27715e065e4a7f24a6c1da3d6f415d8d77
wyk1984 <132546564+wyk1984@users.noreply.github.com>
2023-05-04 15:23:56 +0800

    qjustin3
    question3

--- src/qjustin3.js
+++ src/qjustin3.js
@@ -1,7 +1,6 @@
 /*
     Task 1:
     - Add a new property "breed" with the value "Poodle"
-
     Task 2: 
     - Implement a for-in to loop through the keys in "dog" object and print it with console.log()
 */
@@ -9,7 +8,14 @@
 const dog = {
     petName:"Bobby"
 }
+  petName: "Bobby",
+};
 
 // Task 1: Add code here
 
 // Task 2: Add code here
+dog["breed"] = "Poodle";
+// Task 2: Add code here
+for (const property in dog) {
+  console.log(`${property}:${dog[property]}`);
+}
